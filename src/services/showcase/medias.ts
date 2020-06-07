e06fd4c118d38afdfc00eb00edf52d8751b9f07d
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-06-07 15:51:41 -0300

    feat: interfaces and medias - add interface of matterport object - add array of medias to test

---
+++ src/services/showcase/medias.ts
@@ -0,0 +1,32 @@
+export const paramsMedia = [
+  {
+    anchorPosition: { x: 1.09, y: 1.0, z: 0.122 },
+    stemVector: { x: 0.32, y: 0.55, z: 1.0 },
+    color: {
+      r: 0.0,
+      g: 0.0,
+      b: 0.0,
+    },
+    description: 'Deveria ter aqui o vídeo do Jon Gom',
+    label: 'Jon Gom',
+    media: {
+      src: 'https://www.youtube.com/watch?v=nY7GnAq6Znw',
+      type: 'mattertag.media.video',
+    },
+  },
+  {
+    anchorPosition: { x: 2.09, y: 0.7, z: 0.25 },
+    stemVector: { x: 0.72, y: 0.45, z: 0.5 },
+    color: {
+      r: 0.5,
+      g: 0.2,
+      b: 0.3,
+    },
+    description: 'Mais um vídeo da música Ocean',
+    label: 'John Buttler',
+    media: {
+      src: 'https://www.youtube.com/watch?v=jdYJf_ybyVo',
+      type: 'mattertag.media.video',
+    },
+  },
+];
